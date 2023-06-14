e1d229169e255f7f2bd75bdd8cb9753ceb9518de
ViktorBalan <vtx8bln@gmail.com>
2023-06-14 10:48:41 +0300

    lesson1.2 - вывод фразы 10 раз

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,7 +2,7 @@ package main.java.test;
 
 public class Test {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Волгогра - лучший город на земле");
         }
     }
