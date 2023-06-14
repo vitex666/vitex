4c3bf7f2875f38d76b8f8558701d92551823ea57
ViktorBalan <vtx8bln@gmail.com>
2023-06-14 10:39:32 +0300

    test1_t

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,9 @@
+package main.java.test;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("v");
+        }
+    }
+}
