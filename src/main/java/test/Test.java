ac334bb457ad564ba9071fd47580f9a3826eb805
ViktorBalan <vtx8bln@gmail.com>
2023-06-14 11:12:06 +0300

    lesson1.2 - вывод фразы N-кол-во раз

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,8 +1,13 @@
 package main.java.test;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        System.out.println("Введите количество повторений" + n);
+        for (int i = 0; i < n; i++) {
             System.out.println("Волгогра - лучший город на земле");
         }
     }
