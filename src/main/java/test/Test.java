70031109f7b2d12ec53d4e630fabdbf8e8de968f
ViktorBalan <vtx8bln@gmail.com>
2023-06-14 10:47:19 +0300

    lesson1

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,7 +3,7 @@ package main.java.test;
 public class Test {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println("v");
+            System.out.println("Волгогра - лучший город на земле");
         }
     }
 }
