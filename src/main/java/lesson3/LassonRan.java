de0d1d006735cdefdc642f6ff6a3340517557047
ViktorBalan <vtx8bln@gmail.com>
2023-06-18 12:08:38 +0300

    lesson3 - random(-10...10)

---
+++ src/main/java/lesson3/LassonRan.java
@@ -0,0 +1,24 @@
+package main.java.lesson3;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LassonRan {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите размер массива - ");
+        int n = console.nextInt();
+
+        int [] array = new int[n];
+        System.out.println("заполните масив целых чисел: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int)(Math.random()*20) - 10);
+        }
+        System.out.print("Воть: ");
+        String resultString = IntStream.of(array)
+                .mapToObj(i -> i + "")
+                .collect(Collectors.joining(","));
+        System.out.println(resultString);
+    }
+}
