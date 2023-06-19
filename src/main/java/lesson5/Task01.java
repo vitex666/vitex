c4362322f5693698e51d695c102e56abb96957e9
amaslovskiy <ramzet89@gmail.com>
2023-06-19 14:26:08 +0300

    Task09 - business programming example

--- src/main/java/lesson5/Task01.java
+++ src/main/java/lesson5/Task01.java
@@ -9,20 +9,38 @@ public class Task01 {
         System.out.print("Введите размер массива - ");
         int n = console.nextInt();
 
+        int[] array = createArray(n);
+
+        System.out.print("Вывод: ");
+
+        System.out.println(arrayToString(array));
+        int sum = calculateSum(array);
+        System.out.println(sum);
+    }
+
+    public static int[] createArray(int n) {
         int[] array = new int[n];
-        System.out.println("заполните масив целых чисел: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = ((int) (Math.random() * 30) - 20);
         }
-        System.out.print("Вывод: ");
-        int lastArray = array[array.length - 1];
-        int sumArray = 0;
-        for (int i = 0; i < array.length - 1; i++) {
-            System.out.print(array[i] + ", ");
-            sumArray = sumArray+array[i];
-        }
-        System.out.println(lastArray);
-        System.out.println(sumArray+lastArray);
+        return array;
+    }
 
+    public static int calculateSum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static String arrayToString(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            sb.append(arr[i]).append(',');
+        }
+        sb.append(arr[arr.length - 1]);
+        return sb.toString();
     }
 }
