3afffd6b7951fe3d6a5c44c12b3f6082c82d4b52
ViktorBalan <vtx8bln@gmail.com>
2023-06-19 14:11:05 +0300

    lesson3task01 - random(-20...10) and sumArray_fix

--- src/main/java/task09/Task01.java
+++ src/main/java/task09/Task01.java
@@ -1,16 +1,14 @@
-package main.java.lesson5;
+package main.java.task09;
 
 import java.util.Scanner;
 
 
-public class Task01 {
+public class main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("Введите размер массива - ");
         int n = console.nextInt();
 
-        int[] array = new int[n];
-        System.out.println("заполните масив целых чисел: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = ((int) (Math.random() * 30) - 20);
         }
