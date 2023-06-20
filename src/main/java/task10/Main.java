ff5c9cbe225dc1b243e468df01b41636ad3b55bd
ViktorBalan <vtx8bln@gmail.com>
2023-06-20 17:07:33 +0300

    task10 - array[i, i*2....]fix

--- src/main/java/task10/Main.java
+++ src/main/java/task10/Main.java
@@ -3,27 +3,38 @@ package main.java.task10;
 import java.util.Scanner;
 
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите размер масива - ");
         int n = console.nextInt();
 
-        int [] array = new int[n];
-        int first = 1;
 
-        for (int i = 0; i < array.length; i++) {
-            array [i] = first;
-            first = first * 2;
+        int[] arr = createArray(n);
+        System.out.println("Воть: " + arrayToString(arr));
 
-        }
-        System.out.print("Воть: ");
-        int lA = array[array.length - 1];
-        for (int i = 0; i < array.length-1; i++) {
-            System.out.print(array[i]+", ");
-        }
-        System.out.println(lA);
     }
 
+    public static String arrayToString(int[] array) {
+        int lA = array[array.length - 1];
+        String arr = "";
+        for (int i = 0; i < array.length - 1; i++) {
+            arr = arr + array[i] + ", ";
+        }
+        return arr + lA;
+    }
+
+    public static String doo(String str) {
+        return str;
+    }
+
+    public static int[] createArray(int n) {                    //создание масива
+        int[] array = new int[n];
+        int f = 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = f;
+            f = f * 2;
+        }
+        return array;
+    }
 }
