b05b7c4dc748979bc089ceb2d73123ba8d2f5769
ViktorBalan <vtx8bln@gmail.com>
2023-06-22 15:18:36 +0300

    task10 - array[i-2 + i-1]

---
+++ src/main/java/task11/Main.java
@@ -0,0 +1,35 @@
+package main.java.task11;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите размер масива - ");
+        int n = console.nextInt();
+        int[] arr = createArray(n);
+        System.out.println("Результат: " + arrayToString(arr));
+    }
+
+    public static String arrayToString(int[] arr){   //выводим весь масив в одну строку через запятую
+        int arrayL = arr[arr.length - 1];
+        String a = "";
+        for (int i = 0; i < arr.length; i++) {
+            a = a + arr[i] + ", ";
+        }
+        return a + arrayL;
+    }
+
+    public static int[] createArray(int n){
+        int[] arr = new int[n];
+
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i-2]+arr[i-1];
+
+        }
+        return arr;
+    }
+
+}
