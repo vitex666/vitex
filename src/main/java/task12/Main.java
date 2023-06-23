2c818d1472e8508b950546b091bd99071263b4db
ViktorBalan <vtx8bln@gmail.com>
2023-06-23 17:18:22 +0300

    task12

--- src/main/java/task12/Main.java
+++ src/main/java/task12/Main.java
@@ -3,9 +3,20 @@ package main.java.task12;
 public class Main {
     public static void main(String[] args) {
         int[] array = createArray(10);
+        printArray(array);
+//        int[] array1 = createArray(15);
+//        printArray(array1);
+//        int[] array2 = createArray(20);
+//        printArray(array2);
+//        int[] array3 = createArray(5);
+//        printArray(array3);
 
+
+
+    }
+    public static void printArray (int[] array){
         for (int i = 0; i < array.length; i++) {
-            if (array[i]%2==0) {
+            if (array[i]%2==0){
                 System.out.println(array[i] + " - четное");
             } else {
                 System.out.println(array[i] + " - нечетное");
